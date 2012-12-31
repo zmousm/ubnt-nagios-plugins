b5d3116b942c8a125f957dee46ac0a9d0dceb84f
Zenon Mousmoulas <zmousm@noc.grnet.gr>
2012-12-31 13:46:18 +0200

    Commit second incarnation of mrtg-ubnt-probe script.

--- mrtg-ubnt-probe.py
+++ mrtg-ubnt-probe.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 """ MRTG probe for UBNT devices over HTTP """
-__version__ = "1.0"
+__version__ = "2.0"
 __author__ = "Zenon Mousmoulas"
 
 import os
@@ -19,15 +19,14 @@ from MultiPartForm import MultiPartForm
 from DictDotLookup import DictDotLookup
 
 # Setup argument handler
-parser = OptionParser (usage="Usage: %prog -H <hostname> [-U <username>] -P <password> -s <source> -k <key1> <key2> [options]", description="MRTG probe for UBNT devices (over HTTP)", epilog=None)
+parser = OptionParser (usage="Usage: %prog -H <httphost> [-U <username>] -P <password> -k <source> <key> [options]", description="MRTG probe for UBNT devices (over HTTP)", epilog=None)
 parser.add_option ("-V", "--version", action="store_true", help="show the version and exit")
 parser.add_option ("-v", "--verbose", action="count", help="show debugging information")
 parser.add_option ("-t", "--timeout", type="int", default=10, help="seconds before plugin times out (default: 10)")
 
-dataGroup = OptionGroup (parser, "Options for data source and values returned by the program")
-dataGroup.add_option("-s", "--source", help="Specify the source of data the program will poll. This option expects exactly one (1) argument that will be used to construct the URL to be requested through HTTP from the device: GET /<source>.cgi")
-dataGroup.add_option("-k", "--keys", nargs=2, help="Pick the keys that you want the program to return values for. This option must be followed by exactly two (2) arguments. Any key that appears in the selected data source may be given, using dotted notation to perform nested lookups in JSON data structures. Example: -k airfiber.txcapacity airfiber.rxcapacity")
-dataGroup.add_option("-f", "--formulas", nargs=2, help="Specify optional formulas (expressions to be interepreted by Python using eval()) that shall be used to modify values before they are returned by the program. This option must be followed by exactly two (2) arguments. The order in which the formulas are given corresponds to the order that keys are specified. Any occurences of VAL in a formula will be replaced by the corresponding value. If an empty string is given processing is skipped for the corresponding value. Example: -f \"VAL*1000\" \"VAL/1000\"")
+dataGroup = OptionGroup (parser, "Options for data sources and values returned by the program")
+dataGroup.add_option("-k", "--source-key", nargs=2, action="append", help="This option should be used as many times as the number of values to be returned by the program. It must be followed by exactly two (2) arguments: the source of data the program will poll and the key for the value to be probed. The first argument will be used to construct the URL to be requested through HTTP from the device (GET /<source>.cgi). For the second argument, any key that appears in the selected data source may be given, using dotted notation to perform nested lookups in JSON data structures. Example: -k stats airfiber.txcapacity -k stats airfiber.rxcapacity")
+dataGroup.add_option("-f", "--formula", action="append", help="Specify an optional formula [expression to be interpreted by Python using eval()] that shall be used to modify the corresponding value before it is returned. This option must be used as many times as the number of source-key options, so that formulas are matched with keys. Any occurences of VAL in a formula will be replaced by the corresponding value. If an empty string is given, processing is skipped for the corresponding value. Example: -f \"VAL*1000\" \"VAL/1000\"")
 parser.add_option_group (dataGroup)
 
 connGroup = OptionGroup (parser, "Connection options")
@@ -54,13 +53,11 @@ try:
 	if (options.password is None):
 		parser.error ("-P/--password option is required")
 
-	if (options.source is None):
-		parser.error ("-s/--source option is required")
+	if (options.source_key is None):
+		parser.error ("-k/--source-key option is required")
 
-	datasourceuri = "/%s.cgi" % options.source
-
-	if (options.keys is None):
-		parser.error ("-k/--keys option is required")
+	if ((options.formula is not None) and (len(options.formula) != len(options.source_key))):
+		parser.error ("-f/--formula option must be used as many times as the -k/--source-key option")
 
 	# Prepare login
 	form = MultiPartForm()
@@ -82,56 +79,82 @@ try:
 	if (verbose >= 2):
 		print "Opening session"
 	resp = urlopen(req)
-
 	# Post the form to login
-	form.add_field('uri', datasourceuri)
+	form.add_field('uri', '/index.cgi')
 	body = str(form)
 	req = Request(options.httphost + '/login.cgi')
 	req.add_header('Content-type', form.get_content_type())
 	req.add_header('Content-length', len(body))
 	req.add_data(body)
 	if (verbose >= 2):
-		print "Logging in and collecting data"
+		print "Logging in"
 	resp = urlopen(req)
-
 	# Check we reached the right page after redirection post login
-	if (resp.geturl() != options.httphost + datasourceuri):
+	if (resp.geturl() != options.httphost + '/index.cgi'):
 		if (verbose >= 2):
 			print "Login may have failed"
-		raise Exception("Reached a wrong page: " + resp.geturl())
-
-	# Check content-type (last resort) before passing to JSON parser
-	contype = resp.info()['Content-type']
-	if (contype != "application/json"):
-		if (verbose >= 2):
-			print "Login may have failed"
-		raise Exception("Response has wrong content-type: " + contype)
-
-	data = json.loads(resp.read())
-	data = DictDotLookup(data)
+		raise Exception("reached a wrong page: " + resp.geturl())
 
 	# Avoid leaving open sessions
-	req = Request(options.httphost + '/logout.cgi')
-	resp = urlopen(req)
+	def sessionclose():
+		req = Request(options.httphost + '/logout.cgi')
+		if (verbose >= 2):
+			print "Logging out"
+		resp = urlopen(req)
 
+	# Poll data sources (only once for each data source)
+	data = datasourceuri = {}
+	for source in dict(options.source_key).keys():
+		datasourceuri[source] = "/%s.cgi" % source
+
+		req = Request(options.httphost + datasourceuri[source])
+		if (verbose >= 2):
+			print "Collecting data (%s)" % datasourceuri[source]
+		resp = urlopen(req)
+
+		# Check we reached the right page after redirection post login
+		if (resp.geturl() != options.httphost + datasourceuri[source]):
+			raise Exception("reached a wrong page: " + resp.geturl())
+
+		# Check content-type (last resort) before passing to JSON parser
+		contype = resp.info()['Content-type']
+		if (contype != "application/json"):
+			raise Exception("response has wrong content-type: " + contype)
+
+		d = json.loads(resp.read())
+		d = DictDotLookup(d)
+
+		data[source] = d
+
+	sessionclose()
+
+	if (not len (data)):
+		raise Exception("no valid sources or no data collected from sources")
+
+	# Process keys
 	returndata = []
-	for key in options.keys:
+	for i in range(len(options.source_key)):
+		(source, key) = options.source_key[i]
+
 		if (not len (key)):
-			continue
+			raise Exception("invalid key (empty string)")
 
+		# Attempt to find key in data source
 		try:
-			key_data = eval ('data.' + key)
+			key_data = eval ('data[source].%s' % key)
 		except AttributeError:
-			raise Exception("No key %s found in data source (URL: %s)" % (key, options.httphost + datasourceuri))
+			raise Exception("no key %s found in data source (URL: %s)" % (key, options.httphost + datasourceuri[source]))
 
-		if (options.formulas is not None):
-			formula = options.formulas[options.keys.index(key)]
-			if (len (formula)):
+		# Massage value with formula
+		if (options.formula is not None):
+			formula = options.formula[i]
+			if (formula):
 				formula = formula.replace("VAL", "key_data")
 				key_data = eval (formula)
 
 		returndata.append(key_data)
 
+	# Return values
 	for val in returndata:
 		print val
 
@@ -139,3 +162,6 @@ except Exception, e:
 	if (verbose >= 2):
 		traceback.print_exc(e)
 	print "ERROR: %s" % str(e)
+
+	sessionclose()
+
