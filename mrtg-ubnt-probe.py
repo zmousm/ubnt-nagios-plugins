edcbe186cf28dd342235c6b27e80a6bd7e2ae55b
Zenon Mousmoulas <zmousm@noc.grnet.gr>
2013-01-02 09:18:22 +0200

    mrtg-ubnt-probe.py: Add output format option, currently a placeholder for anything other than the simple format expected by MRTG.

--- mrtg-ubnt-probe.py
+++ mrtg-ubnt-probe.py
@@ -24,11 +24,6 @@ parser.add_option ("-V", "--version", action="store_true", help="show the versio
 parser.add_option ("-v", "--verbose", action="count", help="show debugging information")
 parser.add_option ("-t", "--timeout", type="int", default=10, help="seconds before plugin times out (default: 10)")
 
-dataGroup = OptionGroup (parser, "Options for data sources and values returned by the program")
-dataGroup.add_option("-k", "--source-key", nargs=2, action="append", help="This option should be used as many times as the number of values to be returned by the program. It must be followed by exactly two (2) arguments: the source of data the program will poll and the key for the value to be probed. The first argument will be used to construct the URL to be requested through HTTP from the device (GET /<source>.cgi). For the second argument, any key that appears in the selected data source may be given, using dotted notation to perform nested lookups in JSON data structures. Example: -k stats airfiber.txcapacity -k stats airfiber.rxcapacity")
-dataGroup.add_option("-e", "--expression", action="append", help="Specify an optional expression [to be interpreted by Python using eval()] that shall be used to modify the corresponding value before it is returned. This option must be used as many times as the number of source-key options, so that expressions are matched with keys. Any occurences of VAL in an expression will be replaced by the corresponding value. If an empty string is given, processing is skipped for the corresponding value. Example: -e \"VAL*1000\" \"VAL/1000\"")
-parser.add_option_group (dataGroup)
-
 connGroup = OptionGroup (parser, "Connection options")
 connGroup.add_option ("-H", "--httphost", help="HTTP(S) protocol, hostname, port (optional), as URL, e.g: http://example.com:port, https://example.com etc.")
 parser.add_option_group (connGroup)
@@ -37,6 +32,15 @@ authGroup = OptionGroup (parser, "Authentication options")
 authGroup.add_option ("-U", "--username", default="ubnt", help="username (default: 'ubnt')")
 authGroup.add_option ("-P", "--password", help="password")
 parser.add_option_group (authGroup)
+
+dataGroup = OptionGroup (parser, "Options for data sources and values returned by the program")
+dataGroup.add_option("-k", "--source-key", nargs=2, action="append", help="This option should be used as many times as the number of values to be returned by the program. It must be followed by exactly two (2) arguments: the source of data the program will poll and the key for the value to be probed. The first argument will be used to construct the URL to be requested through HTTP from the device (GET /<source>.cgi). For the second argument, any key that appears in the selected data source may be given, using dotted notation to perform nested lookups in JSON data structures. Example: -k stats airfiber.txcapacity -k stats airfiber.rxcapacity")
+dataGroup.add_option("-e", "--expression", action="append", help="Specify an optional expression [to be interpreted by Python using eval()] that shall be used to modify the corresponding value before it is returned. This option must be used as many times as the number of source-key options, so that expressions are matched with keys. Any occurences of VAL in an expression will be replaced by the corresponding value. If an empty string is given, processing is skipped for the corresponding value. Example: -e \"VAL*1000\" \"VAL/1000\"")
+parser.add_option_group (dataGroup)
+
+outputGroup = OptionGroup (parser, "Options for data output")
+outputGroup.add_option("-f", "--output-format", type="choice", choices=["MRTG"], default="MRTG", help="Choose an output format for values returned by the program, available options are: MRTG (default): Print each value on a new line, MRTG expects two values.")
+parser.add_option_group(outputGroup)
 
 try:
 	(options, args) = parser.parse_args()
@@ -155,8 +159,9 @@ try:
 		returndata.append(key_data)
 
 	# Return values
-	for val in returndata:
-		print val
+	if (options.output_format == "MRTG"):
+		for val in returndata:
+			print val
 
 except Exception, e:
 	if (verbose >= 2):
